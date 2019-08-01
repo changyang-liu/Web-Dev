7d40121b507b07e12cc74c4e6db25053420fc99f
changyang-liu <liuchangyang112@gmail.com>
2019-08-01 17:53:25 +0800

    Added create, show and update routes for restful blog.
    Also added styling for create, show and edit.

--- restful_blog/app.js
+++ restful_blog/app.js
@@ -1,13 +1,15 @@
 var bodyParser = require("body-parser"),
-mongoose = require("mongoose"),
-express = require("express"),
-app = express();
+methodOverride = require("method-override")
+mongoose       = require("mongoose"),
+express        = require("express"),
+app            = express();
 
 //app config
 mongoose.connect("mongodb://localhost/restful_blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"))
 
 //mongodb config
 var blogSchema = new mongoose.Schema({
@@ -19,9 +21,81 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create({
-    title: "Test blog",
-    image: "https://images.unsplash.com/photo-1563729950602-4c24322b6b34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-    body: "This is a testing blog post"
+app.get("/", function(req, res){
+    res.redirect("/blogs");
 });
+
+//index route
+app.get("/blogs", function(req, res){
+    Blog.find({}).sort({created: -1}).exec(function(err, blogs){
+        if(err){
+            console.log("Error loading blogs from database");
+        }else{
+            console.log("Blogs loaded successfully")
+            res.render("index", {blogs:blogs});
+        }
+    });
+});
+
+//new route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//create route
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, blogPost){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//show route
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, blogPost){
+        if(err){
+            console.log(err);
+            //res.redirect("/blogs");
+        }else{
+            res.render("show", {blog: blogPost});
+        }
+    });
+});
+
+//edit route
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, blogPost){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("edit", {blog:blogPost});
+        }
+    });
+});
+
+//update route
+app.put("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, blogPost){
+        if(err){
+            console.log(err);
+            console.log("Error in find by id")
+        }else{
+            blogPost.set(req.body.blog);
+            blogPost.save(function(err, updatedBlog){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect("/blogs/" + req.params.id);
+                    console.log("Error in save")
+                }
+            })
+            
+        }
+    });
+});
+
+app.listen(3000);
 

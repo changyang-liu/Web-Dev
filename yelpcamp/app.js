80c8433ab554c4124365c085d8b30bd044395ca7
liuchangyang <liuchangyang112@gmail.com>
2019-08-07 23:19:14 +0800

    Completed the show route for yelpcamp.

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -2,22 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Campground = require('./models/campground');
+
 
 //Express config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
-//MongoDB Config
+//Mongoose config
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {useNewUrlParser: true});
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 //HOME
 app.get("/", function(req, res){
@@ -30,7 +24,7 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", {campgrounds:allCampgrounds});
         }
     });
 });
@@ -42,8 +36,7 @@ app.get("/campgrounds/new", function(req, res){
 
 //CREATE ROUTE
 app.post("/campgrounds", function(req, res){
-    var newCamp = new Campground(req.body.campground);
-    Campground.create(req.body.Campground, function(err, newCamp){
+    Campground.create(req.body.campground, function(err, newCamp){
         if(err){
             console.log(err);
         }else{
@@ -54,11 +47,11 @@ app.post("/campgrounds", function(req, res){
 
 //SHOW ROUTE
 app.get("/campgrounds/:id", function(req, res){
-    Campground.find(req.params.id, function(err, foundCamp){
+    Campground.findOne({_id: req.params.id}, function(err, foundCamp){
         if(err){
             console.log(err);
         }else{
-            res.render("show", {campground = foundCamp});
+            res.render("show", {campground: foundCamp});
         }
     });
 });
